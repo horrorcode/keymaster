1ab8c13a416ab3a01397027804e67aea48859435
rene <rene@egevej44.dk>
2014-12-20 08:32:57 +0100

    #210 Window to manage command line alias

---
+++ src/Horror.KeyWords/View/MagicWordEditorWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Horror.Keywords.View
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Windows;
+
+  /// <summary>
+  /// Interaction logic for MagicWordEditorWindow.xaml
+  /// </summary>
+  public partial class MagicWordEditorWindow : Window
+  {
+    public MagicWordEditorWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
