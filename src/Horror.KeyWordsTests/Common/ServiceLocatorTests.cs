2e3b96b8ffd0b27fdc035235bab44e7becef64ed
rene <rene@egevej44.dk>
2014-12-20 08:50:06 +0100

    #227 added a unit test project

---
+++ src/Horror.KeyWordsTests/Common/ServiceLocatorTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Horror.Keywords.Tests
+{
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  [TestClass()]
+  public class ServiceLocatorTests
+  {
+    private const string MyConst = "ConstString";
+    #region Public Methods
+
+    [TestMethod()]
+    public void RegisterTest()
+    {
+
+      ServiceLocator.Register<int>(() => 1);
+      Assert.AreEqual(1, ServiceLocator.Resolve<int>());
+    }
+
+    [TestMethod()]
+    public void ResolveTest()
+    {
+
+      ServiceLocator.Register<string>(() => MyConst);
+      Assert.AreEqual(MyConst, ServiceLocator.Resolve<string>());
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(ApplicationException))]
+    public void DuplicateTest()
+    {
+
+      ServiceLocator.Register<string>(() => MyConst);
+      ServiceLocator.Register<string>(() => MyConst);
+
+    }
+
+    #endregion
+
+  }
+}
---
+++ src/Horror.KeyWordsTests/Service/RegistryServiceTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Horror.Keywords.Tests
+{
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  [TestClass()]
+  public class RegistryServiceTests
+  {
+    private const string STR_SOFTWAREMicrosoftWindowsCurrentVersionRun = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+    [TestMethod()]
+    public void RemoveRunOnStartTest()
+    {
+
+
+      using (Microsoft.Win32.RegistryKey Key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(STR_SOFTWAREMicrosoftWindowsCurrentVersionRun, true))
+      {
+        Key.SetValue("test", "testpath");
+        var s = new RegistryService();
+        s.RemoveRunOnStart("test");
+        var test = Key.GetValue("test");
+        Assert.IsNull(test);
+      }
+    }
+
+    [TestMethod()]
+    public void RunOnStartTest()
+    {
+      var s = new RegistryService();
+      s.RunOnStart("test", "testpath");
+
+      using (Microsoft.Win32.RegistryKey Key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(STR_SOFTWAREMicrosoftWindowsCurrentVersionRun, true))
+      {
+        var test = Key.GetValue("test");
+        Assert.IsNotNull(test);
+        Key.DeleteValue("test");
+      }
+    }
+  }
+}
