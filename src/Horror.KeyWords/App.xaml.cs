37ea5a52c97f0d0bf210b59cb581de57318f917c
rene <rene@egevej44.dk>
2014-12-20 07:01:16 +0100

    #219 Adding solution files

---
+++ src/Horror.KeyWords/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿namespace Horror.Keywords
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Windows;
+
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the App class.
+    /// </summary>
+    public App()
+    {
+      ServiceRegistration();
+      InitializeComponent();
+      Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Handles the DispatcherUnhandledException event of the Current control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="System.Windows.Threading.DispatcherUnhandledExceptionEventArgs"/> instance containing the event data.</param>
+    private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+      using (var page = new System.Windows.Forms.ThreadExceptionDialog(e.Exception))
+        page.ShowDialog();
+    }
+
+    /// <summary>
+    /// Services the registration.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+    private static void ServiceRegistration()
+    {
+      var hotkeyService = new HotkeyService();
+      var keywordService = new KeywordService();
+
+      MagicWordContainer magicWordContainer = keywordService.LoadMagicWords();
+
+      var mainViewModel = new MainViewModel(magicWordContainer, hotkeyService, keywordService);
+      ServiceLocator.Register<IMainViewModel>(() => mainViewModel);
+      ServiceLocator.Register<IKeywordService>(() => keywordService);
+      ServiceLocator.Register<IHotkeyService>(() => hotkeyService);
+      ServiceLocator.Register<IRegistryService>(() => new RegistryService());
+    }
+
+    #endregion
+
+  }
+}
---
+++ src/Horror.KeyWords/Properties/AssemblyInfo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Horror.Keywords")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Horror.Keywords")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+  //(used if a resource is not found in the page, 
+  // or application resource dictionaries)
+ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+  //(used if a resource is not found in the page, 
+  // app, or any theme specific resource dictionaries)
+)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Horror.KeywordsTests, PublicKey=AAAAB3NzaC1yc2EAAAADAQABAAABAQC/23eTdqJ8zTyf5jETowQvAgWqr5Lgpwl/QZPpoOovM3ezfmEvUXq9vfOO7jvF3iG62d95hnGnji6TKpiUpXgN+ABMxBfounbAua7cdlLt4muHFBddClTwBPB5xvBpCjHy9Y2LICZb7MYWNyucv9uaIyjSTdshjhHmUUGmiE143S5pmN/FZ+HikNzO96M1ojOC9O5rUYhPt6aUq2nlvZJRWznYfyiXCVWfDE4Tbg0a1ej/lGuPKF5Nobspku54PYEV1BjM5fZDXZPCmQS1YLgCl9fzocnk81Mk1G3dWkyKhPQiwBucer3z58HfyhpAhvEhjCP3yJT3eH6/LfQwwA9Z")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: CLSCompliantAttribute(true)]
