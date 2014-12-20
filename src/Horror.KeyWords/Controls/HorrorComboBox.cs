a5ae3804f77b8e078dc4ec64b40d510f3c13d1ee
rene <rene@egevej44.dk>
2014-12-20 08:48:52 +0100

    properly going to be used

---
+++ src/Horror.KeyWords/Controls/HorrorComboBox.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Horror.Keywords
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Windows;
+  using System.Windows.Controls;
+  using System.Windows.Data;
+
+  public class HorrorComboBox : ComboBox
+  {
+    #region Public Methods
+
+    public override void OnApplyTemplate()
+    {
+      base.OnApplyTemplate();
+
+      TextBox textBox = (TextBox)Template.FindName("PART_EditableTextBox", this);
+
+      // Create a template-binding in code  
+      Binding binding = new Binding("ContextMenu");
+      binding.RelativeSource = new RelativeSource(RelativeSourceMode.TemplatedParent);
+      BindingOperations.SetBinding(textBox,
+          FrameworkElement.ContextMenuProperty, binding);
+    }
+
+    #endregion
+
+  }
+}
