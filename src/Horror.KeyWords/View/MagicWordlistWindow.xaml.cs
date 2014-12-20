c6523ece8951f62f868b50564bf8044ab532ccd7
rene <rene@egevej44.dk>
2014-12-20 07:43:52 +0100

    #207 Window that gives an overview of command alias

---
+++ src/Horror.KeyWords/View/MagicWordlistWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Horror.Keywords.View
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Windows;
+
+  /// <summary>
+  /// Interaction logic for MagicWordlistWindow.xaml
+  /// </summary>
+  public partial class MagicWordlistWindow : Window
+  {
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MagicWordlistWindow"/> class.
+    /// </summary>
+    public MagicWordlistWindow()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+
+  }
+}
