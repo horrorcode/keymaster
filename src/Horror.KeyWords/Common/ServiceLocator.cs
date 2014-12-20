17c61f4f0516aae8596aa1fba63f947bb3bb2d44
rene <rene@egevej44.dk>
2014-12-20 08:36:56 +0100

    #220 Implemented a service locator

---
+++ src/Horror.KeyWords/Common/ServiceLocator.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Horror.Keywords
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  /// <summary>
+  /// 
+  /// </summary>
+  static public class ServiceLocator
+  {
+    #region Fields
+
+    private readonly static Dictionary<Type, Func<object>> services = new Dictionary<Type, Func<object>>();
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Registers the specified resolver.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="resolver">The resolver.</param>
+    public static void Register<T>(Func<T> resolver)
+    {
+      ServiceLocator.services[typeof(T)] = () => resolver();
+    }
+
+    /// <summary>
+    /// Resets this instance.
+    /// </summary>
+    public static void Reset()
+    {
+      ServiceLocator.services.Clear();
+    }
+
+    /// <summary>
+    /// Resolves this instance.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T Resolve<T>()
+    {
+      return (T)ServiceLocator.services[typeof(T)]();
+    }
+
+    #endregion
+
+  }
+}
