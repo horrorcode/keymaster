76753b9e2c3026d38821df64ec40daaaee26896a
rene <rene@egevej44.dk>
2014-12-20 07:44:54 +0100

    #206 Window that enables the user to change settings for the command line tool

---
+++ src/Horror.KeyWords/View/SettingsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Horror.Keywords.View
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Windows;
+
+  /// <summary>
+  /// Interaction logic for SettingsWindow.xaml
+  /// </summary>
+  public partial class SettingsWindow : Window
+  {
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SettingsWindow"/> class.
+    /// </summary>
+    public SettingsWindow()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+
+  }
+}
