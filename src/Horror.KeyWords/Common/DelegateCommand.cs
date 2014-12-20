5edc1bd84c2b8d7043adb5be357604647ccbe434
rene <rene@egevej44.dk>
2014-12-20 08:02:13 +0100

    #221 Common mvvm functionality

---
+++ src/Horror.KeyWords/Common/DelegateCommand.cs
@@ -0,0 +1,103 @@
+﻿namespace Horror.Keywords
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Globalization;
+  using System.Linq;
+  using System.Windows.Input;
+
+  /// <summary>
+  ///
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public class DelegateCommand<T> : ICommand
+    where T: class
+  {
+    #region Fields
+
+    private readonly Predicate<T> _canExecute;
+
+    private readonly Action<T> _execute;
+
+    #endregion
+
+    #region Events
+
+    /// <summary>
+    /// Occurs when changes occur that affect whether or not the command should execute.
+    /// </summary>
+    public event EventHandler CanExecuteChanged;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DelegateCommand{T}"/> class.
+    /// </summary>
+    /// <param name="execute">The execute.</param>
+    public DelegateCommand(Action<T> execute)
+      : this(execute, null)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DelegateCommand{T}"/> class.
+    /// </summary>
+    /// <param name="execute">The execute.</param>
+    /// <param name="canExecute">The can execute.</param>
+    public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+    {
+      _execute = execute;
+      _canExecute = canExecute;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Defines the method that determines whether the command can execute in its current state.
+    /// </summary>
+    /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+    /// <returns>
+    /// true if this command can be executed; otherwise, false.
+    /// </returns>
+    public bool CanExecute(object parameter)
+    {
+      if (_canExecute == null)
+      {
+        return true;
+      }
+      return _canExecute((T)parameter);
+    }
+
+    /// <summary>
+    /// Defines the method to be called when the command is invoked.
+    /// </summary>
+    /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+    public void Execute(object parameter)
+    {
+      if (_execute != null)
+      {
+        T castParameter = (T)Convert.ChangeType(parameter, typeof(T), CultureInfo.InvariantCulture);
+        _execute(castParameter);
+      }
+    }
+
+    /// <summary>
+    /// Raises the can execute changed.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate")]
+    public void RaiseCanExecuteChanged()
+    {
+      if (CanExecuteChanged != null)
+      {
+        CanExecuteChanged(this, EventArgs.Empty);
+      }
+    }
+
+    #endregion
+
+  }
+}
---
+++ src/Horror.KeyWords/Common/EventToCommandBehavior.cs
@@ -0,0 +1,163 @@
+namespace Horror.Keywords
+{
+  using System;
+  using System.Globalization;
+  using System.Reflection;
+  using System.Windows;
+  using System.Windows.Input;
+  using System.Windows.Interactivity;
+
+  /// <summary>
+  /// Behavior that will connect an UI event to a viewmodel Command,
+  /// allowing the event arguments to be passed as the CommandParameter.
+  /// </summary>
+  public class EventToCommandBehavior : Behavior<FrameworkElement>
+  {
+
+    #region Readonly
+
+    public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command", typeof(ICommand), typeof(EventToCommandBehavior), new PropertyMetadata(null));
+
+    public static readonly DependencyProperty EventProperty = DependencyProperty.Register("Event", typeof(string), typeof(EventToCommandBehavior), new PropertyMetadata(null, OnEventChanged));
+
+    public static readonly DependencyProperty PassArgumentsProperty = DependencyProperty.Register("PassArguments", typeof(bool), typeof(EventToCommandBehavior), new PropertyMetadata(false));
+
+    #endregion
+
+    #region Fields
+
+    private Delegate _handler;
+
+    private EventInfo _oldEvent;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the command.
+    /// </summary>
+    /// <value>
+    /// The command.
+    /// </value>
+    public ICommand Command
+    {
+      get
+      {
+        return (ICommand)GetValue(CommandProperty);
+      }
+      set
+      {
+        SetValue(CommandProperty, value);
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets the event.
+    /// </summary>
+    /// <value>
+    /// The event.
+    /// </value>
+    public string Event
+    {
+      get
+      {
+        return (string)GetValue(EventProperty);
+      }
+      set
+      {
+        SetValue(EventProperty, value);
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether [pass arguments].
+    /// </summary>
+    /// <value>
+    ///   <c>true</c> if [pass arguments]; otherwise, <c>false</c>.
+    /// </value>
+    public bool PassArguments
+    {
+      get
+      {
+        return (bool)GetValue(PassArgumentsProperty);
+      }
+      set
+      {
+        SetValue(PassArgumentsProperty, value);
+      }
+    }
+
+    #endregion
+
+    #region Protected Methods
+
+    /// <summary>
+    /// Called after the behavior is attached to an AssociatedObject.
+    /// </summary>
+    /// <remarks>
+    /// Override this to hook up functionality to the AssociatedObject.
+    /// </remarks>
+    protected override void OnAttached()
+    {
+      AttachHandler(Event);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Attaches the handler to the event
+    /// </summary>
+    private void AttachHandler(string eventName)
+    {
+      if (_oldEvent != null)
+        _oldEvent.RemoveEventHandler(AssociatedObject, _handler);
+      if (!string.IsNullOrEmpty(eventName))
+      {
+        var ei = AssociatedObject.GetType().GetEvent(eventName);
+        if (ei != null)
+        {
+          var mi = GetType().GetMethod("ExecuteCommand", BindingFlags.Instance | BindingFlags.NonPublic);
+          _handler = Delegate.CreateDelegate(ei.EventHandlerType, this, mi);
+          ei.AddEventHandler(AssociatedObject, _handler);
+          _oldEvent = ei;
+        }
+        else
+          throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "The event '{0}' was not found on type '{1}'", eventName, AssociatedObject.GetType().Name));
+      }
+    }
+
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    private void ExecuteCommand(object sender, EventArgs e)
+    {
+      var parameter = PassArguments ? e : null;
+      if (Command != null)
+      {
+        if (Command.CanExecute(parameter))
+          Command.Execute(parameter);
+      }
+    }
+
+    /// <summary>
+    /// Called when [event changed].
+    /// </summary>
+    /// <param name="d">The d.</param>
+    /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
+    private static void OnEventChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      var beh = (EventToCommandBehavior)d;
+
+      if (beh.AssociatedObject != null)
+        beh.AttachHandler((string)e.NewValue);
+    }
+
+    #endregion
+
+  }
+}
